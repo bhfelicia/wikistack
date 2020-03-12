7f0af06c358f53b39dbef69f8b798f606ca2b32b
Jennifer Chen <jenniferchen95@gmail.com>
2020-03-12 13:02:31 -0400

    schemas

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const { db } = require('./models');
+const layout = require('./views/layout')
+
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send(layout(""))
 })
-const PORT = 1337;
+
+
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+db.authenticate().
+then(() => {
+  console.log('Connected to the database');
+})
---
+++ models/index.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+
+  }
+})
+
+
+module.exports = {
+  db
+}
